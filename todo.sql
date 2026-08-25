0c487203d68f23c4fcb9c61428d02ea5d3e978ca
Ved Vyas <vedcodes2312@gmail.com>
2026-08-25 19:52:16 +0530

    Create todo.sql

---
+++ todo.sql
@@ -0,0 +1,8 @@
+-- aggregation assignment
+CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
+INSERT INTO todo_list VALUES (2, "vacuuming", 20);
+INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+INSERT INTO todo_list VALUES (4,"attend class",60);
+
+SELECT SUM(minutes) FROM todo_list;
